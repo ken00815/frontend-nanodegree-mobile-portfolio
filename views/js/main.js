66bc782278e2ac73a02dc5dd2e8b05d04f7bc8b4
Ken <KenandChristie@christidkensmbp.lan>
2018-03-07 10:45:39 +0800

    no message

--- views/js/main.js
+++ views/js/main.js
@@ -492,9 +492,10 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   // document.body.scrollTop is no longer supported in Chrome.
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  var set = (scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
 
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(set + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     
   }
